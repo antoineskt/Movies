0e70e40629b57422171bf41f2916e1e3a6d3cb21
antoineskt <billet.antoine42@gmail.com>
2023-10-24 18:34:30 +0200

    second commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import logocine from '../assets/logocine.png';
+
+export function Header() {
+  return (
+    <div className="flex flex-row ">
+      <img src={logocine} alt="logo ciné" className="w-32"></img>yo
+    </div>
+  );
+}
